5b38b1e4a40a333fafbfbd19da78811026a1d0cd
Deepak1229276 <deepak.student.min24@itbhu.ac.in>
2026-02-15 04:13:47 +0530

    vid

--- app/components/WalletConnectProvider.js
+++ app/components/WalletConnectProvider.js
@@ -2,26 +2,32 @@ import { useMemo } from 'react'
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import { GlowWalletAdapter, PhantomWalletAdapter, SlopeWalletAdapter, SolflareWalletAdapter, TorusWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { clusterApiUrl } from '@solana/web3.js'
 
+// Import individual wallet adapters
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { GlowWalletAdapter } from '@solana/wallet-adapter-glow'
+import { SlopeWalletAdapter } from '@solana/wallet-adapter-slope'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
+import { TorusWalletAdapter } from '@solana/wallet-adapter-torus'
+
 export const WalletConnectProvider = ({ children }) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
     const network = WalletAdapterNetwork.Devnet
 
-    // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => {
         if (network === WalletAdapterNetwork.Devnet) {
             return 'https://api.devnet.solana.com'
         }
-
         return clusterApiUrl(network)
     }, [network])
 
-    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-    // Only the wallets you configure here will be compiled into your application, and only the dependencies
-    // of wallets that your users connect to will be loaded.
-    const wallets = useMemo(() => [new PhantomWalletAdapter(), new GlowWalletAdapter(), new SlopeWalletAdapter(), new SolflareWalletAdapter({ network }), new TorusWalletAdapter()], [network])
+    const wallets = useMemo(() => [
+        new PhantomWalletAdapter(), 
+        new GlowWalletAdapter(), 
+        new SlopeWalletAdapter(), 
+        new SolflareWalletAdapter({ network }), 
+        new TorusWalletAdapter()
+    ], [network])
 
     return (
         <ConnectionProvider endpoint={endpoint}>
